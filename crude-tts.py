3c0e837d08313d17cb5c015d08eb7710333b4bda
Nils Carlson <nilscarlson1997@gmail.com>
2023-10-09 07:07:19 -0400

    Seperate adding markers and adding speach, allow greater creative control of timing

--- crude-tts.py
+++ crude-tts.py
@@ -104,7 +104,7 @@ def split_dialogue_to_segments(dialogue, max_letters=20):
         word_ended = char in " " and len(segment) > max_letters
 
         if line_ended or word_ended:
-            segments.append(segment.replace("\n", "").strip())
+            segments.append(segment.strip())
             segment = ''
 
     if segment:  # For the final segment
@@ -115,7 +115,7 @@ def create_text(segment):
     # Create the text object
     bpy.ops.object.text_add()
     text_obj = bpy.context.object
-    text_obj.data.body = segment
+    text_obj.data.body = segment.replace("\n", "")
 
     # Force an update of the scene
     # (without this, the obj will read the default 'text'
@@ -139,6 +139,10 @@ def create_text(segment):
         text_obj.data.materials[0] = material
     else:
         text_obj.data.materials.append(material)
+
+    # Add to subtitles collection
+    bpy.context.collection.objects.unlink(text_obj)
+    bpy.data.collections['subtitles'].objects.link(text_obj)
     return text_obj
 
 def keyframe_subtitle_visibility(text_obj, frame, visible: bool, scale_value=(0.1, 0.1, 0.1)):
@@ -149,15 +153,21 @@ def keyframe_subtitle_visibility(text_obj, frame, visible: bool, scale_value=(0.
     text_obj.keyframe_insert(data_path="scale", frame=frame)
 
 def set_marker(name, frame):
-    marker = bpy.context.scene.timeline_markers.new("#"+name, frame=frame)
+    marker = bpy.context.scene.timeline_markers.new("#"+name, frame=int(frame))
 
-def clear_markers(prefix="#"):
-    scene = bpy.context.scene
-    markers_to_remove = [
-        m for m in scene.timeline_markers
+def get_segment_markers(prefix="#"):
+    """
+    Return only the markers that are used for speech
+    e.g., those prefixed with '#'
+    """
+    return [
+        m for m in bpy.context.scene.timeline_markers
         if m.name.startswith(prefix)
     ]
-    for m in markers_to_remove:
+
+def clear_markers():
+    scene = bpy.context.scene
+    for m in get_segment_markers():
         scene.timeline_markers.remove(m)
 
 def clear_collection(collection_name):
@@ -189,29 +199,69 @@ def clear_mouth_keyframes(mouth):
 def clear_vse():
     bpy.context.window.workspace = bpy.data.workspaces["Video Editing"]
     bpy.context.scene.sequence_editor_clear()
-
-def main(dialogue, directory, letter_duration, start_frame=20, audio=False):
+    
+def get_duration_frames(letter_duration):
     """
-    Create subtitles, animate mouth shapes, and add audio
+    Given a time (in seconds) we want each letter to roughtly last,
+    return the # of frames, at the current framerate, each letter would last
     """
+    # TODO could switch to a system where it is based on dialoge length and project length?
     framerate = bpy.context.scene.render.fps
-    # Convert duration into frames
     duration_frames = int(letter_duration * framerate)
-    current_frame = start_frame
     if duration_frames < 1:
-        print(f"{letter_duration} duration and {framerate} framerate caused {duration_frames} duration_frames")
-        return
+        raise ValueError(f"{letter_duration} duration and {framerate} framerate caused {duration_frames} duration_frames")
+    return duration_frames
+
+def get_segment_length(segment, duration_frames=1):
+    """
+    Given a segment string, and how many frames we want each
+    'letter' to take to say, return the total number of frames
+    this segment will last
+    - given that we pause longer on punctuation
+    """
+    frames = 0
+    for char in segment:
+        char = char.lower()
+        frames += duration_frames
+        if char in '.,!?':
+            frames += 5 * duration_frames
+    return frames
+
+def add_markers(dialogue, frame_margins=20):
+    """Split the dialogue into segments and place markers for each segment."""
+    start_frame = frame_margins
+    last_frame = bpy.context.scene.frame_end - frame_margins
+    talking_frames = last_frame - start_frame
+
+    # Clear any previous markers
+    clear_markers()
+
+    current_frame = start_frame
+    segments = split_dialogue_to_segments(dialogue)
+    frames_per_marker = talking_frames / len(segments)
+
+    for segment in segments:
+        # Marker the start of a segment
+        set_marker(segment, current_frame)
+        current_frame += frames_per_marker
+        
+def add_speech_to_markers(dialogue, directory, audio=False):
+    """
+    Create subtitles, animate mouth shapes, and add audio,
+    based on the markers present from
+    """
+    markers = get_segment_markers()
+    segments = split_dialogue_to_segments(dialogue)
 
     # Get the 'mouth' object
     mouth = bpy.data.objects.get('mouth')
     if not mouth:
-        print("No 'mouth' object found.")
-        return
+        raise ValueError("No 'mouth' object found.")
 
     # Clear any data from a previous run
-    clear_markers()
     clear_collection('subtitles')
     clear_mouth_keyframes(mouth)
+    # Remove previous strips from VSE
     if (audio):
         clear_vse()
 
@@ -223,95 +273,112 @@ def main(dialogue, directory, letter_duration, start_frame=20, audio=False):
         subtitles_collection = bpy.data.collections['subtitles']
 
     # Start the mouth as 'at rest'
-    set_rest_mouth(mouth, current_frame-1, duration_frames)
+    start_frame = markers[0].frame
+    set_rest_mouth(mouth, start_frame-1, 1)
 
-    # Iterate over each subtitle segment in the dialogue
-    segments = split_dialogue_to_segments(dialogue)
-    for idx, segment in enumerate(segments):
-        # Create text object for the segment
+    # Iterate through each segment/marker, making 
+    assert len(markers) == len(segments)
+    for i in range(len(segments)):
+        # Use marker's frame as starting point
+        segment = segments[i]
+        marker = markers[i]
+        next_marker = markers[i+1] if i+1 < len(markers) else None
+
+        segment_start = marker.frame
+        segment_end = next_marker.frame if next_marker else bpy.context.scene.frame_end
+
+        # How many frames we get to spend on each letter
+        duration_frames = (segment_end - segment_start) // get_segment_length(segment)
+        print(f"Starting: {segment_start}-{segment_end} ({duration_frames}) {segment}")
+        # TODO for now, just make sure it can't be 0 - we will cut it off
+        # if needed
+        duration_frames = max(1, duration_frames)
+
+        # Create and set up text object for this segment
         text_obj = create_text(segment)
-        clear_nla_data(text_obj)
-        
-        # Link text object to 'subtitles' collection
-        bpy.context.collection.objects.unlink(text_obj)
-        subtitles_collection.objects.link(text_obj)
-        
         # All subtitles can be 'visible' on frame 0
         # (to ease editing in viewport)
         keyframe_subtitle_visibility(text_obj, 0, True)
         # But they are invisible from 1 to just before they are needed
         keyframe_subtitle_visibility(text_obj, 1, False)
-        keyframe_subtitle_visibility(text_obj, current_frame-1, False)
+        keyframe_subtitle_visibility(text_obj, segment_start-1, False)
         # And becomes visible at the start of this segment
-        keyframe_subtitle_visibility(text_obj, current_frame, True)
+        keyframe_subtitle_visibility(text_obj, segment_start, True)
+        # And becomes invisible again after end of segment
+        keyframe_subtitle_visibility(text_obj, segment_end-1, True)
+        keyframe_subtitle_visibility(text_obj, segment_end, False)
 
-        # Marker the start of a segment for easier keyframe editing
-        set_marker(segment, current_frame)
-
-        # For each character in segment, do mouth and audio
+        # Animate mouth and optionally load audio for each character
+        current_frame = segment_start
         for char in segment:
             char = char.lower()
-            
-            # By default, have mouth at rest
             set_rest_mouth(mouth, current_frame, duration_frames)
-            
-            # Check character type
+
             if char in 'abcdefghijklmnopqrstuvwxyz':
-                if(audio):
+                if audio:
                     load_letter_audio(directory, char, current_frame, duration_frames)
                 set_mouth(mouth, char, current_frame, duration_frames)
-                    
-                current_frame += duration_frames
-            elif char == ' ':
-                current_frame += duration_frames
-            elif char in '.,!?':
-                current_frame += 6 * duration_frames
+            
+            current_frame += duration_frames
+            if char in '.,!?':
+                current_frame += 5 * duration_frames
+            # TODO for now, if we go over, just go to next
+            if current_frame >= segment_end:
+                break
 
-        # Subtitle becomes invisible again after end of segment
-        keyframe_subtitle_visibility(text_obj, current_frame-1, True)
-        keyframe_subtitle_visibility(text_obj, current_frame, False)
-
+    # Finish up
     bpy.context.window.workspace = bpy.data.workspaces["Layout"]
-
     # For now, let's select these new objects
     bpy.ops.object.select_all(action='DESELECT')
     mouth.select_set(True)
     for text_obj in subtitles_collection.objects:
         text_obj.select_set(True)
 
+def main(redo_markers=False):
+    # Update with your desired dialogue, directory path, letter_duration:
+    dialogue = """
+    Is this running? Yes.
+    Listen close - I'll speak quickly because I don't have much tape.
 
-# Update with your desired dialogue, directory path, letter_duration:
-dialogue = """
-Is this running? Yes.
-Listen close - I'll speak quickly because I don't have much tape.
+    And I don't know how many of these I can safely.. Distribute.
 
-And I don't know how many of these I can safely.. Distribute.
+    Okay. You asked:
+    "In communism, is there a currency? How is it managed?"
 
-Okay. You asked:
-"In communism, is there a currency? How is it managed?"
+    Historically, the Soviet Union, China, and Cuba all danced with currencies, every twist orchestrated by the authoritarian state.
 
-Historically, the Soviet Union, China, and Cuba all danced with currencies, every twist orchestrated by the authoritarian state.
+    But ah, mon ami, let us indulge in a little fantasy, shall we? Imagine, if you will, an ideal communist society – 
+    "ideal", but let's keep one foot in reality. 
 
-But ah, mon ami, let us indulge in a little fantasy, shall we? Imagine, if you will, an ideal communist society – "ideal", but let's keep one foot in reality. 
+    "From each according to his ability, to each according to his needs"
+    - a beautiful mantra, non?
+    A library economy - use what you need, borrow what you want, return when you are done.
+    No money needed. But how do we get there?
 
-"From each according to his ability, to each according to his needs" - a beautiful mantra, non? A library economy - use what you need, borrow what you want, return when you are done. No money needed. But how do we get there?
+    Marx whispered about 'Labour Vouchers'. Given for your hours of toil, they are spent once and then... poof, vanished, no chance for hoarding. Today, these would be digital, decentralized, perhaps blockchain. But it has much the same stench as cash.
 
-Marx whispered about 'Labour Vouchers'. Given for your hours of toil, they are spent once and then... poof, vanished, no chance for hoarding. Today, these would be digital, decentralized, perhaps blockchain. But it has much the same stench as cash.
+    And trading with the outside world? Ah, it gets murky. Fixed rate agreements, or anchored to commodities, diversified reserves - perhaps I will save it for another day.
 
-And trading with the outside world? Ah, it gets murky. Fixed rate agreements, or anchored to commodities, diversified reserves - perhaps I will save it for another day.
+    "Participatory Economics" touts the strength of decentralized planning - letting the communities vote on how their stockpiles are filled, trying to sidestep the bureaucratic failures of the past.
 
-"Participatory Economics" touts the strength of decentralized planning - letting the communities vote on how their stockpiles are filled, trying to sidestep the bureaucratic failures of the past.
+    And economies should not be written in stone, by men in gray suits. The feedback systems need to be in a constant dance. If vouchers are in play, then whenever shortage is reported, the value grows in real time. No rigid central planning, no hoarding, no monopoly.
 
-And economies should not be written in stone, by men in gray suits. The feedback systems need to be in a constant dance. If vouchers are in play, then whenever shortage is reported, the value grows in real time. No rigid central planning, no hoarding, no monopoly.
+    But ah, the challenges. Even transitory vouchers risk inflation - and black-markets - but I will save that for another time.
 
-But ah, the challenges. Even transitory vouchers risk inflation - and black-markets - but I will save that for another time.
+    Remember, my friend, even in this imagined paradise, currency's dance has many steps. Eh, voilà!
 
-Remember, my friend, even in this imagined paradise, currency's dance has many steps. Eh, voilà!
+    Eh. We didn't answer anything yet, but my tape is running out. Please, submit your follow up questions, I will do my best to answer what I can.
 
-Eh. We didn't answer anything yet, but my tape is running out. Please, submit your follow up questions, I will do my best to answer what I can.
+    For now, au revoir.
+    """
+    directory = "F:\\My-Blender\\blender-other\\Strings Attached - Puppet Code\\squeaks\\"
 
-For now, au revoir.
-"""
-directory = "F:\\My-Blender\\blender-other\\Strings Attached - Puppet Code\\squeaks\\"
-letter_duration = 0.08
-main(dialogue, directory, letter_duration)
+    # If we don't have markers, just add those, and let the user move them depending on
+    # how they want the pacing.
+    if (get_segment_markers() == [] or redo_markers == True):
+        add_markers(dialogue)
+    else:
+        add_speech_to_markers(dialogue, directory)
+
+# TODO WATCH REDO MARKERS
+main()
